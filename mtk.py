64a4c4528efcfecb50b22564f4b3dfdfe9032cb3
Keyy <52876644+naufal212@users.noreply.github.com>
2019-07-26 19:46:23 +0700

    Aritmatika.py
    tools matematika

---
+++ mtk.py
@@ -0,0 +1,41 @@
+# file aritmatika2.py
+
+import os, sys, time
+print ("=============================")
+print "Author : Naufal"
+time.sleep(0.1)
+print "Project : Aritmatika"
+time.sleep(0.1)
+print ("=============================")
+# Ambil input untuk mengisi nilai
+a = input("Masukkan angka pertama :")
+b = input("Masukkan angka kedua :")
+print ("=============================")
+# Menggunakan Operator Penjumlahan 
+c = a + b
+print "Hasil %d + %d = %d" % (a,b,c)
+print
+# Menggunakan Operator Pengurangan
+c = a - b
+print "Hasil %d - %d = %d" % (a,b,c)
+print
+# Menggunakan Operator Pembagian
+c = a / b
+print "Hasil %d / %d = %d" % (a,b,c)
+print
+# Menggunakan Operator Perkalian
+c = a * b
+print "Hasil %d * %d = %d" % (a,b,c)
+print
+# Menggunakan Operator Sisa Bagi
+c = a * b
+print "Hasil %d %% %d = %d" % (a,b,c)
+print
+# Menggunakan Operator Pangkat
+c = a * b
+print "Hasil %d ** %d = %d" % (a,b,c)
+print ("==============================") 
+
+
+
+
